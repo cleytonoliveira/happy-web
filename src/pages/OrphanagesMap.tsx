36d024322de1c664f428579f2ffa57588430beee
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-10-14 01:22:05 -0300

    feat: add map in a page

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
 
 import '../styles/pages/orphanages-map.css';
+import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
@@ -22,13 +24,21 @@ class OrphanagesMap extends Component {
             <strong>Curitiba</strong>
             <span>Paraná</span>
           </footer>
-
-          <div></div>
-          
-          <Link to="/" className="create-orphanage">
-            <FiPlus size={32} color="#FFF" />
-          </Link>
         </aside>
+
+        <Map
+          center={[-25.3863465, -49.2470175]}
+          zoom={15}
+          style={{ width: '100%', height: '100%' }}
+        >
+          <TileLayer
+            url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+          />
+        </Map>
+
+        <Link to="/" className="create-orphanage">
+          <FiPlus size={32} color="#FFF" />
+        </Link>
       </div>
     )
   }
