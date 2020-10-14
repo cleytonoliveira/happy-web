91494f2c084a67a3873051b51759a46837ea148e
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-10-13 22:10:04 -0300

    feat: create content of orphanages page

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../images/map-marker.svg';
 
 class OrphanagesMap extends Component {
   render() {
     return (
-      <div>teste</div>
+      <div className="page-map" >
+        <aside>
+          <header>
+            <img src={mapMarkerImg} alt="Happy Marker" />
+
+            <h2>Escolha um orfanato no mapa</h2>
+            <p>Muitas crianças estão esperando a sua visita :)</p>
+          </header>
+
+          <footer>
+            <strong>Curitiba</strong>
+            <span>Paraná</span>
+          </footer>
+
+          <div>MAPA</div>
+          <Link to="/" className="create-orphanage">
+            <FiPlus size={32} color="#FFF" />
+          </Link>
+        </aside>
+      </div>
     )
   }
 }
